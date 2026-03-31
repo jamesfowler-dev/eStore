257d8ccaf6c48d70e3e66b6c88ea1e4877630ad0
jamesfowler-dev <james_fowler21@hotmail.com>
2026-03-31 11:40:54 +0100

    add footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
-import { APP_NAME } from "@/lib/constants";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "@/assets/styles/globals.css";
+// import { APP_NAME } from "@/lib/constants";
 import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
 
 
-const inter = Inter({
-  subsets: ["latin"]
-});
+// const inter = Inter({
+//   subsets: ["latin"]
+// });
 
 
-export const metadata: Metadata = {
-  title: `${APP_NAME}`,
-  description: "A modern ecommerce platform built with Next.js",
-};
+// export const metadata: Metadata = {
+//   title: `${APP_NAME}`,
+//   description: "A modern ecommerce platform built with Next.js",
+// };
 
 export default function RootLayout({
   children,
@@ -24,6 +25,7 @@ export default function RootLayout({
     <div className="flex h-screen flex-col">
         <Header />
         <main className="flex-1 wrapper">{children}</main>
+        <Footer />
     </div>
   );
 }
