e3e353f7f8470c6a57879342ef4ba1f3990122fd
jamesfowler-dev <james_fowler21@hotmail.com>
2026-04-13 15:37:18 +0100

    add sign-up functionality

--- lib/validators.ts
+++ lib/validators.ts
@@ -34,3 +34,14 @@ export const insertProductSchema = () =>
         email: z.string().email('Invalid email address'),
         password: z.string().min(6, 'Password must be at least 6 characters')
     });
+
+    // Scheme for signing up a user 
+    export const signUpFormSchema = z.object({
+        name: z.string().min(3, "Name must be at least 3 characters"),
+        email: z.string().email('Invalid email address'),
+        password: z.string().min(6, 'Password must be at least 6 characters'),
+        confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters'),
+    }).refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ['confirmPassword']
+    });
