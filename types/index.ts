51970e5b464e42c6cda6bfb73a659f7c0d98639f
jamesfowler-dev <james_fowler21@hotmail.com>
2026-04-14 12:00:32 +0100

    add zod schema and prisma model for cart

--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,11 @@
-export type Product = {
+import { z } from 'zod';
+import { insertProductSchema, insertCartSchema, cartItemSchema } from '@/lib/validators';
+
+export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
-    name: string;
-    slug: string;
-    category: string;
-    images: string[];
-    brand: string;
-    description: string;
-    stock: number;
-    price: string;
     rating: string;
-    numReviews: number;
-    isFeatured: boolean;
-    banner: string | null;
     created_at: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
