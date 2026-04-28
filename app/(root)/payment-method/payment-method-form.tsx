52f33ab4087a504fba501f2d024d3011e0577091
James Fowler <james_fowler21@JamesFosMBP2024.powerhub>
2026-04-28 17:13:44 +0100

    add payment method form with radiogroup

--- app/(root)/payment-method/payment-method-form.tsx
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -3,18 +3,24 @@ import { useRouter } from "next/navigation";
 import { toast, useSonner } from "sonner";
 import { useTransition } from "react";
 import { paymentMethodSchema } from "@/lib/validators";
-import CheckoutSteps from "@/components/shared/checkout-steps";
 import { useForm } from "react-hook-form";
 import { z } from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { DEFAULT_PAYMENT_METHOD } from "@/lib/constants";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+import { RadioGroupItem, RadioGroup } from "@/components/ui/radio-group";
+import { PAYMENT_METHODS } from "@/lib/constants";
+import { updateUserPaymentMethod } from "@/lib/actions/user.actions";
 
-const PaymentMethodForm = ({ preferredPaymentMethod }: {
+const PaymentMethodForm = ({
+    preferredPaymentMethod,
+}: {
     preferredPaymentMethod: string | null
 }) => {
 
     const router = useRouter();
-    const { toast } = useSonner();
 
     const form = useForm<z.infer<typeof paymentMethodSchema>>({
         resolver: zodResolver(paymentMethodSchema),
@@ -25,10 +31,83 @@ const PaymentMethodForm = ({ preferredPaymentMethod }: {
 
     const [isPending, startTransition] = useTransition();
 
+    const onSubmit = async (values: z.infer<typeof paymentMethodSchema>) => {
+        startTransition(async () => {
+            const res = await updateUserPaymentMethod(values);
+
+            if (!res.success) {
+                toast(res.message, { variant: 'destructive' });
+                return;
+            }
+            router.push('/place-order');
+        })
+    }
+
     return (
-        <>
-            <CheckoutSteps current={2} />
-        </>
+        <div className="max-w-md mx-auto space-y-4">
+            <h1 className="h2-bold mt-4">
+                Payment Method
+            </h1>
+            <p className="text-sm text-muted-foreground">
+                Please select a payment method
+            </p>
+            <Form {...form}>
+                <form
+                    method='POST'
+                    className='space-y-4'
+                    onSubmit={form.handleSubmit(onSubmit)}
+                >
+                    <div className="flex flex-col md:flex-row gap-5">
+                        <FormField
+                            control={form.control}
+                            name='type'
+                            render={({ field }) => (
+                                <FormItem className='space-y-3'>
+                                    <FormControl>
+                                        <RadioGroup
+                                            value={field.value}
+                                            onValueChange={field.onChange}
+                                            className='flex flex-col space-y-2'
+                                        >
+                                            {PAYMENT_METHODS.map((paymentMethod) => (
+                                                <FormItem
+                                                    key={paymentMethod}
+                                                    className='flex items-center space-x-3 space-y-0'
+                                                >
+                                                    <FormControl>
+                                                        <RadioGroupItem
+                                                            value={paymentMethod}
+                                                        />
+                                                    </FormControl>
+                                                    <FormLabel className='font-normal'>
+                                                        {paymentMethod}
+                                                    </FormLabel>
+                                                </FormItem>
+
+                                            ))}
+                                        </RadioGroup>
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className="flex gap-2">
+                        <Button
+                            type='submit'
+                            className="rounded-xl"
+                            disabled={isPending}>
+                            {isPending ? (
+                                <Loader className='w-4 h-4 animate-spin' />
+                            ) : (
+                                <ArrowRight className='w-4 h-4' />
+                            )} {' '}
+                            Continue
+                        </Button>
+                    </div>
+                </form>
+            </Form>
+        </div >
     );
 }
 
