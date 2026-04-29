90b373d3b6dba5b8adb9417a04eb032f70d38c82
James Fowler <james_fowler21@JamesFosMBP2024.powerhub>
2026-04-29 15:43:54 +0100

    add zod schemas and types

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
-import { 
-    insertProductSchema, 
-    insertCartSchema, 
-    cartItemSchema, 
-    shippingAddressSchema 
+import {
+    insertProductSchema,
+    insertCartSchema,
+    cartItemSchema,
+    shippingAddressSchema,
+    insertOrderItemSchema,
+    insertOrderSchema
 } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -22,4 +24,15 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    created_at: Date;
+    isPaid: Boolean;
+    paidAt: Date | null;
+    isDelivered: Boolean;
+    deliveredAt: Date | null;
+    orderItems: OrderItem[];
+    user: { name: string; email: string };
+};
 
